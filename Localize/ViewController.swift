d5426dad55ec820c36a5aeeeb4f4ffca1ebb9210
Kerem Demir <keremdemir1694@gmail.com>
2023-09-17 21:18:21 +0300

    Start

---
+++ Localize/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Localize
+//
+//  Created by Kerem Demir on 17.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
